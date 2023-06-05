5cf81f678e27728538ef025fe1e6dd6fe082af42
Sofia Contreras <cgsofiaelizabeth15@gmail.com>
2023-06-05 12:47:44 -0400

    all the funtionalities

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -18,7 +18,7 @@ function renderSchedule(){
   // applying the past, present, or future class to each time
   for (var i = 0; i < 9; i++) {
 
-    timeBlock = timeContainer.children('div').eq(i);
+    var timeBlock = timeContainer.children('div').eq(i);
     blockId = i + 9;
 
     if(actualHour < blockId){
@@ -29,20 +29,40 @@ function renderSchedule(){
       timeBlock.addClass('present');
     }  
   }
+
   getInput();
 };
 
 function getInput(){
- // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this? 
+   // render the the description saved un the local storage
+  var tasks = JSON.parse(localStorage.getItem('description'));
+
+  for (var i = 0; i < tasks.length; i++){
+    var timeBlock = timeContainer.children('div').eq(i);
+    if(tasks[i] != null){
+      timeBlock.children('textarea').text(tasks[i].description)
+    }
+  } 
 };
 
 saveBtn.on('click', function () {
-   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // save the entry data in the local storage
+  txTarea = $(this.parentNode).children('textarea');
+  entryData = txTarea.val().trim();
+  id = $(this.parentNode).attr('id');
+  
+  const timeBlock = {
+    description : entryData,
+    id : parseInt(id)
+  };
+
+  var description = JSON.parse(localStorage.getItem('description'));
+  
+  if(description == null)
+    description = Array();
+
+  description[timeBlock.id] = timeBlock;
+  localStorage.setItem('description', JSON.stringify(description));
+  
+  getInput();
 });
